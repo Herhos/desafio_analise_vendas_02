335c75471c39de83e326d3af424fd83cfda7ad37
Herhos <herhos@outlook.com>
2023-04-03 11:17:15 -0300

    Desafio Análise de Vendas 02

---
+++ src/application/Program.java
@@ -0,0 +1,78 @@
+package application;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Scanner;
+import entities.Sale;
+
+public class Program
+{
+	// C:\EclipseProjetos\desafio_analise_vendas_02\in.csv
+	
+	public static void main(String[] args)
+	{
+		Locale.setDefault(Locale.US);
+		Scanner scn = new Scanner(System.in);
+		
+		System.out.print("Entre o caminho do arquivo: ");
+		String caminho = scn.nextLine();
+		
+		try (BufferedReader bfr = new BufferedReader(new FileReader(caminho)))
+		{			
+			List<Sale> lista = new ArrayList<>();			
+			
+			String linha = bfr.readLine();
+			
+			while (linha != null)
+			{
+				String[] campos = linha.split(",");
+				lista.add(new Sale(
+					Integer.parseInt(campos[0]),     // mês
+					Integer.parseInt(campos[1]),     // ano
+					campos[2],                       // vendedor
+					Integer.parseInt(campos[3]),     // itens
+					Double.parseDouble(campos[4]))); // total
+				linha = bfr.readLine();									
+			}
+			
+			System.out.println();
+			
+			Map<String, Double> mapa = new HashMap<>();
+			
+			for (Sale venda : lista)
+			{
+				mapa.put(venda.getSeller(), 0.0);
+			}
+			
+			for (String vendedor : mapa.keySet())
+			{
+				Double total = lista.stream()
+					.filter(s -> s.getSeller().equals(vendedor))
+					.map(s -> s.getTotal())
+					.reduce(0.0, (x,y) -> x + y);
+				mapa.put(vendedor, total);
+			}
+			
+			System.out.println("Total de vendas por vendedor:");
+			
+			for (String vendedor : mapa.keySet())
+			{
+				System.out.printf(vendedor + " - R$ %.2f \n", mapa.get(vendedor));
+			}									
+		}
+		catch (IOException e)
+		{
+			System.out.println("Erro: " + caminho + " (O sistema não pode encontrar o arquivo especificado)");
+		}
+		finally
+		{
+			scn.close();
+		}
+	}
+}
---
+++ src/entities/Sale.java
@@ -0,0 +1,116 @@
+package entities;
+
+import java.util.Objects;
+
+public class Sale
+{
+	private Integer month;
+	private Integer year;
+	private String seller;
+	private Integer items;
+	private Double total;
+	
+	public Sale(String seller, Double total)
+	{
+		this.seller = seller;		
+		this.total = total;
+	}
+	
+	public Sale(Integer month, Integer year, String seller, Integer items, Double total)
+	{
+		this.month = month;
+		this.year = year;
+		this.seller = seller;
+		this.items = items;
+		this.total = total;
+	}
+
+	public Integer getMonth()
+	{
+		return month;
+	}
+
+	public void setMonth(Integer month)
+	{
+		this.month = month;
+	}
+
+	public Integer getYear()
+	{
+		return year;
+	}
+
+	public void setYear(Integer year)
+	{
+		this.year = year;
+	}
+
+	public String getSeller()
+	{
+		return seller;
+	}
+
+	public void setSeller(String seller)
+	{
+		this.seller = seller;
+	}
+
+	public Integer getItems()
+	{
+		return items;
+	}
+
+	public void setItems(Integer items)
+	{
+		this.items = items;
+	}
+
+	public Double getTotal()
+	{
+		return total;
+	}
+
+	public void setTotal(Double total)
+	{
+		this.total = total;
+	}
+	
+	public Double averagePrice()
+	{
+		return total / items;
+	}	
+
+	@Override
+	public int hashCode()
+	{
+		return Objects.hash(seller);
+		//items, month, year, total
+	}
+
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Sale other = (Sale) obj;
+		return Objects.equals(seller, other.seller);
+		//Objects.equals(items, other.items) && Objects.equals(month, other.month)
+		//&& Objects.equals(year, other.year) && Objects.equals(total, other.total) 
+	}
+
+	@Override
+	public String toString()
+	{
+		StringBuilder sbd = new StringBuilder();
+		sbd.append(month + "/" + year);
+		sbd.append(", " + seller);
+		sbd.append(", " + items);
+		sbd.append(", " + String.format("%.2f", total));
+		sbd.append(", pm = " + String.format("%.2f", averagePrice()));
+		return sbd.toString();
+	}
+}
